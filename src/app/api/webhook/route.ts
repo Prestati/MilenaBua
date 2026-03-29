30dccb1e2773ed268478923be2410354cfa3a213
Karoline <karoline@Karoline-sin-MacBook-Air.local>
2026-03-29 21:50:25 +0200

    fix: fjern apiVersion fra Stripe

--- src/app/api/webhook/route.ts
+++ src/app/api/webhook/route.ts
@@ -85,7 +85,6 @@ export async function POST(req: Request) {
     const body = await req.text();
 
     const Stripe = (await import("stripe")).default;
-    const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", { apiVersion: "2025-01-27.acacia" });
 
     let event;
     try {
